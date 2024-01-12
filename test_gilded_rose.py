91dfe368f59b571c15d4b10c4c86ff0c8cea8617
AgathePoulain <139390887+AgathePoulain@users.noreply.github.com>
2024-01-12 16:16:40 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:test_gilded_rose.py

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -11,6 +11,43 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual("foo", items[0].name)
 
+    def test_qualite_positive(self):
+        item = Item("foo", 5, 10)
+        items = [item]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertTrue(item.quality >= 0, "La qualité de l'item doit être positive")
+
+    def test_qualite_limite_max (self):
+        item = Item("foo", 5, 10)
+        items = [item]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertTrue(item.quality <= 50, "La qualité de l'item ne doit pas dépasser 50")
+
+    def test_qualite_sulfuras(self):
+        item = Item("Sulfuras, Hand of Ragnaros", 5, 80)
+        items = [item]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertTrue(item.quality == 80, "La qualité du Sulfuras doit toujours être de 80")
+
+    def test_peremption_passee(self):
+        item = Item("foo", 0, 10)
+        items = [item]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertTrue(item.quality == 8, "La qualité doit se dégrader deux fois plus vite car la date de péremption est passée")
+
+    def test_aged_brie_updated_quality(self):
+        item = Item("Aged Brie", 5, 10)
+        items = [item]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertTrue(item.quality == 11, "La qualité du Aged Brie augmente plus le temps passe")
+
+    
 
 if __name__ == "__main__":
     unittest.main()
+
